c175bc98e6826c65d48e62cb680a5c126c6dac59
Roman Volosatovs <rvolosatovs@riseup.net>
2021-01-28 22:00:47 +0100

    build.rs: add

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::{env, path::PathBuf};
+
+use autotools;
+
+fn main() {
+    println!(
+        "cargo:rustc-link-search=native={}",
+        autotools::Config::new("src/libfvad")
+            .reconf("-iv")
+            .build()
+            .join("lib")
+            .display()
+    );
+    println!("cargo:rustc-link-lib=static=fvad");
+    println!("cargo:rustc-link-lib=c");
+
+    bindgen::Builder::default()
+        .header("src/libfvad/include/fvad.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Failed to generate bindings")
+        .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .expect("Failed to write bindings");
+}
